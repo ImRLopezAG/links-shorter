a89a2d6f6969a9f1e5e025432262438e5c7f5e4e
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-09-03 22:33:54 -0400

    Refactor database connection and update dependencies

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,10 @@
-import { createClient, type Client } from '@libsql/client'
-import { drizzle } from 'drizzle-orm/libsql'
+import Database from 'better-sqlite3'
+import { drizzle } from 'drizzle-orm/better-sqlite3'
 import { env } from 'node:process'
 import * as schema from './schema'
 
-const dbUrl = env.DB_URL ?? 'file:./src/db/db.sqlite'
+const dbUrl = env.DB_URL ?? './src/db/db.sqlite'
 
-export const client = createClient({ url: dbUrl })
+export const client = new Database(dbUrl)
 
 export const db = drizzle(client, { schema })
