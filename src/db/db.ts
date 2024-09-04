b60249780a7b7522086f1ed8d5904ba8046c21fa
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-09-03 22:22:47 -0400

    Refactor image service and update database URL handling

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,11 +4,7 @@ import { env } from 'node:process'
 import * as schema from './schema'
 
 const dbUrl = env.DB_URL ?? 'file:./src/db/db.sqlite'
-const globalForDb = globalThis as unknown as {
-  client: Client | undefined
-}
 
-export const client = globalForDb.client ?? createClient({ url: dbUrl })
-if (process.env.NODE_ENV !== 'production') globalForDb.client = client
+export const client = createClient({ url: dbUrl })
 
 export const db = drizzle(client, { schema })
