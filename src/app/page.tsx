858763e143d61f8e2d554faf7b52036a3c7fb5b4
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-09-06 07:56:48 -0400

    Refactor link in Home component to use a div instead of an anchor tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,8 @@ export default function Home() {
           Welcome to Shurl
         </p>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none'>
-          <a
+          <div
             className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
             rel='noopener noreferrer'
           >
             <Image
@@ -22,7 +20,7 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
+          </div>
         </div>
       </div>
 
